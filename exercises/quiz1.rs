ff0b0a13097c7af5223b62695005781a0ee470f0
optke3 <optke3@gmail.com>
2023-03-25 18:54:50 +0000

    do 25 rustlings exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(i: u64) -> u64 {
+    if i <= 40 {
+        2*i
+    } else {
+        i
+    }
+}
 
 // Don't modify this function!
 #[test]
