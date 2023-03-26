4dd0d1b1ef834f44eaf4bfe457e51a4d7ee229cf
optke3 <optke3@gmail.com>
2023-03-26 02:50:21 +0000

    48.9%

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -40,6 +38,27 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        // idiomatic Rust chaining approach, with closures, and_modify, and or_insert
+        // Rust hashmaps use insert, not add
+        // https://doc.rust-lang.org/std/collections/struct.HashMap.html#method.entry
+        scores.entry(team_1_name.clone()).and_modify(|x| {x.goals_scored += team_1_score; x.goals_conceded+=team_2_score}).or_insert(Team{name: team_1_name.clone(), goals_scored: team_1_score, goals_conceded: team_2_score});
+        scores.entry(team_2_name.clone()).and_modify(|x| {x.goals_scored += team_2_score; x.goals_conceded+=team_1_score}).or_insert(Team{name: team_2_name.clone(), goals_scored: team_2_score, goals_conceded: team_1_score});
+
+        // if let Some(team_1) = scores.get_mut(&team_1_name){
+        //     team_1.goals_scored = team_1.goals_scored + team_1_score;
+        //     team_1.goals_conceded = team_1.goals_conceded + team_2_score;
+        // } else{
+        //     scores.insert(team_1_name.clone(), Team{name: team_1_name.clone(), goals_scored: team_1_score, goals_conceded: team_2_score});
+        // };
+
+        // if let Some(team_2) = scores.get_mut(&team_2_name){
+        //     team_2.goals_scored = team_2.goals_scored + team_2_score;
+        //     team_2.goals_conceded = team_2.goals_conceded + team_1_score;
+        // } else{
+        //     scores.insert(team_2_name.clone(), Team{name: team_2_name.clone(), goals_scored: team_2_score, goals_conceded: team_1_score});
+        // };
+
     }
     scores
 }
